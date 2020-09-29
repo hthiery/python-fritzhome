1575933c31ce8c8910462a61cb2890ef4d2d58a9
refinedcranberry <3584606+refinedcranberry@users.noreply.github.com>
2020-09-29 13:37:26 +0000

    drop support for python27 and python35 as both are deprecated

--- setup.py
+++ setup.py
@@ -48,10 +48,7 @@ setup(
         "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",  # noqa: E501
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
