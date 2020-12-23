a8b22baa242ea9d5ea71d6c356bc7b44ff2921dd
BenPru <32298537+BenPru@users.noreply.github.com>
2020-12-23 15:54:11 +0100

    Add type button
    Add type button for Fritz!DECT 440

--- pyfritzhome/fritzhomedevicefeatures.py
+++ pyfritzhome/fritzhomedevicefeatures.py
@@ -2,12 +2,17 @@ from enum import IntFlag
 
 
 class FritzhomeDeviceFeatures(IntFlag):
-    ALARM = 0x0010
-    UNKNOWN = 0x0020
-    THERMOSTAT = 0x0040
-    POWER_METER = 0x0080
-    TEMPERATURE = 0x0100
-    SWITCH = 0x0200
-    DECT_REPEATER = 0x0400
-    MICROPHONE = 0x0800
-    HANFUN = 0x2000
+    # Bit 0: HAN-FUN Gerät
+    # Bit 2: Licht/Lampe
+    ALARM = 0x0010          # Bit 4: Alarm-Sensor
+    BUTTON = 0x0020         # Bit 5: AVM-Button
+    THERMOSTAT = 0x0040     # Bit 6: Heizkörperregler
+    POWER_METER = 0x0080    # Bit 7: Energie Messgerät
+    TEMPERATURE = 0x0100    # Bit 8: Temperatursensor
+    SWITCH = 0x0200         # Bit 9: Schaltsteckdose
+    DECT_REPEATER = 0x0400  # Bit 10: AVM DECT Repeater
+    MICROPHONE = 0x0800     # Bit 11: Mikrofon
+    HANFUN = 0x2000         # Bit 13: HAN-FUN-Unit
+    # Bit 15: an-/ausschaltbares Gerät/Steckdose/Lampe/Aktor
+    # Bit 16: Gerät mit einstellbarem Dimm-, Höhen- bzw. Niveau-Level
+    # Bit 17: Lampe mit einstellbarer Farbe/Farbtemperatur
