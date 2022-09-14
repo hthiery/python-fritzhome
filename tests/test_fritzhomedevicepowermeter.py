96d8267e1f17207cc1cb578e96960bf2c327424a
mib1185 <mail@mib85.de>
2022-09-14 14:42:06 +0000

    run black linter and add test

--- tests/test_fritzhomedevicepowermeter.py
+++ tests/test_fritzhomedevicepowermeter.py
@@ -27,7 +27,7 @@ class TestFritzhomeDevicePowermeter(object):
         eq_(device.get_switch_power(), 18000)
         device._fritz._request.assert_called_with(
             "http://10.0.0.1/webservices/homeautoswitch.lua",
-            {"ain": u"08761 0000434", "switchcmd": "getswitchpower", "sid": None},
+            {"ain": "08761 0000434", "switchcmd": "getswitchpower", "sid": None},
         )
 
     def test_get_switch_energy(self):
@@ -42,5 +42,19 @@ class TestFritzhomeDevicePowermeter(object):
         eq_(device.get_switch_energy(), 2000)
         device._fritz._request.assert_called_with(
             "http://10.0.0.1/webservices/homeautoswitch.lua",
-            {"ain": u"08761 0000434", "switchcmd": "getswitchenergy", "sid": None},
+            {"ain": "08761 0000434", "switchcmd": "getswitchenergy", "sid": None},
         )
+
+    def test_get_switch_powermeter_properties(self):
+        self.mock.side_effect = [
+            Helper.response("powermeter/device_list"),
+            "2000",
+        ]
+
+        self.fritz.update_devices()
+        device = self.fritz.get_device_by_ain("08761 0000434")
+
+        eq_(device.energy, 707)
+        eq_(device.power, 1000)
+        eq_(device.voltage, 230000)
+        eq_(device.current, 4.3478260869565215)
