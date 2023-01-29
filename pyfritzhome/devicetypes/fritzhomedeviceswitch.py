17be23494afe50691bb940b8b95f105907b02e27
MattBavaria <123907612+MattBavaria@users.noreply.github.com>
2023-01-30 00:40:29 +0100

    added support for Magenta Zwischenstecker
    Magenta Zwischenstecker are HAN-FUN devices and use the "simpleonoff" tag to get/set status.
    I reused mostly the routines already implemented for lightbulbs.
    Fritz DECT 200/210 identify themselves as "switchable device",besides their extended switch functionality too.
    So it had to be assured, that "simpleonoff" is exclusivesly used for HAN-FUN devices.
    Code works for me with HomeAssistant 2022.6 on WDMYEX2ULTRA.
    Could not check, if there is any  unintented effect to lightbulbs - maybe theses classes could be more unified later on.

--- pyfritzhome/devicetypes/fritzhomedeviceswitch.py
+++ pyfritzhome/devicetypes/fritzhomedeviceswitch.py
@@ -15,6 +15,7 @@ class FritzhomeDeviceSwitch(FritzhomeDeviceBase):
     switch_state = None
     switch_mode = None
     lock = None
+    simple_switch = None
 
     def _update_from_node(self, node):
         super()._update_from_node(node)
@@ -28,38 +29,66 @@ class FritzhomeDeviceSwitch(FritzhomeDeviceBase):
     @property
     def has_switch(self):
         """Check if the device has switch function."""
-        return self._has_feature(FritzhomeDeviceFeatures.SWITCH)
+        self.simple_switch = not self._has_feature(FritzhomeDeviceFeatures.SWITCH)
+        self.simple_switch = self.simple_switch and self._has_feature(FritzhomeDeviceFeatures.SWITCHABLE)
+        return (self._has_feature(FritzhomeDeviceFeatures.SWITCH) or self.simple_switch)
 
     def _update_switch_from_node(self, node):
-        val = node.find("switch")
-        try:
-            self.switch_state = self.get_node_value_as_int_as_bool(val, "state")
-        except Exception:
-            self.switch_state = None
-        self.switch_mode = self.get_node_value(val, "mode")
-        try:
-            self.lock = self.get_node_value_as_int_as_bool(val, "lock")
-        except Exception:
-            self.lock = None
+        if not self.simple_switch :
 
-        # optional value
-        try:
-            self.device_lock = self.get_node_value_as_int_as_bool(val, "devicelock")
-        except Exception:
-            pass
+            val = node.find("switch")
+            try:
+                self.switch_state = self.get_node_value_as_int_as_bool(val, "state")
+            except Exception:
+                self.switch_state = None
+            self.switch_mode = self.get_node_value(val, "mode")
+            try:
+                self.lock = self.get_node_value_as_int_as_bool(val, "lock")
+            except Exception:
+                self.lock = None
+
+            # optional value
+            try:
+                self.device_lock = self.get_node_value_as_int_as_bool(val, "devicelock")
+            except Exception:
+                pass
+
+        else :
+            val = node.find("simpleonoff")
+            try:
+                self.switch_state = self.get_node_value_as_int_as_bool(val, "state")
+            except ValueError:
+                pass
 
     def get_switch_state(self):
-        """Get the switch state."""
-        return self._fritz.get_switch_state(self.ain)
+        if not self.simple_switch :
+            """Get the switch state."""
+            return self._fritz.get_switch_state(self.ain)
+        else :
+            return self.switch_state
 
     def set_switch_state_on(self):
-        """Set the switch state to on."""
-        return self._fritz.set_switch_state_on(self.ain)
+        if not self.simple_switch :
+            """Set the switch state to on."""
+            return self._fritz.set_switch_state_on(self.ain)
+        else :
+           self.switch_state = True
+           self._fritz.set_state_on(self.ain)
+
 
     def set_switch_state_off(self):
-        """Set the switch state to off."""
-        return self._fritz.set_switch_state_off(self.ain)
+        if not self.simple_switch :
+            """Set the switch state to off."""
+            return self._fritz.set_switch_state_off(self.ain)
+        else :
+            self.switch_state = True
+            self._fritz.set_state_off(self.ain)
 
     def set_switch_state_toggle(self):
-        """Toggle the switch state."""
-        return self._fritz.set_switch_state_toggle(self.ain)
+        if not self.simple_switch :
+            """Toggle the switch state."""
+            return self._fritz.set_switch_state_toggle(self.ain)
+        else :
+           self.switch_state = True
+           self._fritz.set_state_toggle(self.ain)
+
